c5c0236440fec666ca61852e165e21545eb39eb1
Nghiatran2603 <107810710+NghiaTV2603@users.noreply.github.com>
2023-02-20 18:44:48 +0700

    open with electron

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL('http://localhost:3001/authen/login');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
